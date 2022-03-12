dd67e1b9d2c960eb690932d0d4959a8895ae735e
tolawho <tolawho@gmail.com>
2022-03-12 16:28:10 +0700

    hardhat typescrypt template

--- hhtpl/scripts/deploy.ts
+++ hhtpl/scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+import { ethers } from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -11,22 +11,20 @@ async function main() {
   //
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
-  await hre.run("compile");
+  // await hre.run('compile');
 
   // We get the contract to deploy
-  const Contract = await hre.ethers.getContractFactory("Contract");
-  const contract = await Contract.deploy();
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  await contract.deployed();
+  await greeter.deployed();
 
-  console.log("Contract deployed to:", contract.address);
+  console.log("Greeter deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ hhtpl/test/index.ts
@@ -0,0 +1,31 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+import { Greeter } from "../typechain";
+
+describe("Greeter",  () => {
+  let deployer, contract: Greeter;
+  beforeEach(async () => {
+    [deployer] = await ethers.getSigners();
+    const Contract = await ethers.getContractFactory(
+      "Greeter",
+      deployer
+    );
+    contract = await Contract.deploy("Hello, world!");
+    await contract.deployed();
+  });
+  describe("TestCase", async () => {
+    it("Should return the new greeting once it's changed", async function () {
+
+      expect(await contract.greet()).to.equal("Hello, world!");
+
+      const setGreetingTx = await contract.setGreeting("Hola, mundo!");
+
+      // wait until the transaction is mined
+      await setGreetingTx.wait();
+
+      expect(await contract.greet()).to.equal("Hola, mundo!");
+    });
+  });
+
+});
