a19f17c838b31e805a4e8915e747b09bff49cc9d
tolawho <tolawho@gmail.com>
2022-03-20 09:25:19 +0700

    fix format for project

--- by-example/interface/contracts/Counter.sol
+++ by-example/interface/contracts/Counter.sol
@@ -2,25 +2,25 @@
 pragma solidity ^0.8.9;
 
 contract Counter {
-    uint256 public count;
+	uint256 public count;
 
-    function inc() external {
-        count += 1;
-    }
+	function inc() external {
+		count += 1;
+	}
 }
 
 interface ICounter {
-    function count() external view returns (uint256);
+	function count() external view returns (uint256);
 
-    function inc() external;
+	function inc() external;
 }
 
 contract MyCounter {
-    function incrementCounter(address _counter) external {
-        ICounter(_counter).inc();
-    }
+	function incrementCounter(address _counter) external {
+		ICounter(_counter).inc();
+	}
 
-    function getCount(address _counter) external view returns (uint256) {
-        return ICounter(_counter).count();
-    }
+	function getCount(address _counter) external view returns (uint256) {
+		return ICounter(_counter).count();
+	}
 }
