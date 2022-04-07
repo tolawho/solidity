8b19abe92132c3b790c244c1f4e2cca549291f69
tolawho <tolawho@gmail.com>
2022-04-07 09:13:36 +0700

    eip721

---
+++ by-example/applications/erc721/test/erc721.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+import { Greeter } from "../typechain";
+
+describe("ERC721",  () => {
+  let deployer: SignerWithAddress, caller: SignerWithAddress, contract: Greeter;
+  beforeEach(async () => {
+    [deployer, caller] = await ethers.getSigners();
+    const Contract = await ethers.getContractFactory(
+      "Greeter",
+      deployer
+    );
+    contract = await Contract.deploy("Hello, world!");
+    await contract.deployed();
+  });
+  describe("TestCase", async () => {
+    it("Should ...", async () => {});
+  });
+
+});
